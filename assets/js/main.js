7ab49a4409a5898dcc504510725eed1bbff19c54
adrianlopezgue <adrian.lopez.gue@hotmail.com>
2019-11-24 18:42:26 +0100

    Information about me added

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,11 +63,11 @@ jQuery(document).ready(function($) {
     );
     
     /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
-    new GitHubCalendar("#github-graph", "IonicaBizau");
+    new GitHubCalendar("#github-graph", "AdrianLopezGue");
     
     
     /* Github Activity Feed - https://github.com/caseyscarborough/github-activity */
-    GitHubActivity.feed({ username: "mdo", selector: "#ghfeed" });
+    GitHubActivity.feed({ username: "AdrianLopezGue", selector: "#ghfeed" });
 
 
 });
